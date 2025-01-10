d7e4f85a80275bcbd1a587894a1ab15651be61c5
Silviu Vlad Oprea <silviu@Ancas-MacBook-Air.local>
2025-01-10 13:28:30 +0000

    Quartz sync: Jan 10, 2025, 1:28 PM

--- publishing/convert.sh
+++ publishing/convert.sh
@@ -1,3 +1,3 @@
 jupyter nbconvert \
-  mlpr/mlpr2020/2\ Generalisation.ipynb \
+  mlpr/mlpr2020/4\ Bayesian\ Linear\ Regression.ipynb \
   --to markdown --output-dir quartz/content/Machine\ Learning/
