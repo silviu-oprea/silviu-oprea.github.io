f6b428dc5ca68e4fe5b83f0c612b55b6b56ad735
Silviu Vlad Oprea <silviu@Silvius-MacBook-Pro.local>
2025-07-30 10:11:56 +0100

    Quartz sync: Jul 30, 2025, 10:11 AM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -30,8 +30,8 @@ export const defaultContentPageLayout: PageLayout = {
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.Graph(),
     Component.Backlinks(),
   ],
 }
